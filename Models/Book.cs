6233387c0f3cc24bc92f082fc8ebaf9c27582bcf
Pleasure Ndhlovu <2017269214@ufs4life.ac.za>
2021-07-11 17:54:58 +0200

    Book.cs repositories added

---
+++ Models/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningCoding.Models
+{
+    public class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BookId { get; set; }
+
+        [Required]
+        [DisplayName("Title")]
+        public string BookTitle { get; set; }
+
+        [Required]
+        [DisplayName("Image url")]
+        public string BookUrlImage { get; set; }
+
+        [Required]
+        [DisplayName("Language")]
+        public string BookLanguage { get; set; }
+
+        [Required]
+        [DisplayName("Year")]
+        public string BookYear { get; set; }
+
+        [Required]
+        [DisplayName("Rating")]
+        [Range(1, 5)]
+        public int BookRating { get; set; }
+
+        [Required]
+        [DisplayName("Download url")]
+        [Range(1, 5)]
+        public string BookDownloadUrl { get; set; }
+    }
+}
